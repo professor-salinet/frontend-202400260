9b5b5d3d8627d743354682a8cb1836396dde859f
Projects repositories of Professor Salinet (SENAC) <rodrigo.silva@docente.pr.senac.br>
2024-08-21 11:50:45 -0300

    Add files via upload

---
+++ TextFieldFrame.java
@@ -0,0 +1,62 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class TextFieldFrame extends JFrame 
+{
+   private final JTextField textField1;
+   private final JTextField textField2;
+   private final JTextField textField3;
+   private final JPasswordField passwordField;
+
+   public TextFieldFrame()
+   {
+      super("Testing JTextField and JPasswordField");
+      setLayout(new FlowLayout());
+
+      textField1 = new JTextField(10); 
+      add(textField1);
+
+      textField2 = new JTextField("Enter your text here now");
+      add(textField2);
+
+      textField3 = new JTextField("Uneditable text field", 21);
+      textField3.setEditable(false);
+      add(textField3);
+
+      passwordField = new JPasswordField("Hidden text");
+      add(passwordField);
+
+      TextFieldHandler handler = new TextFieldHandler();
+      textField1.addActionListener(handler);
+      textField2.addActionListener(handler);
+      textField3.addActionListener(handler);
+      passwordField.addActionListener(handler);
+   }
+
+   private class TextFieldHandler implements ActionListener 
+   {
+      @Override
+      public void actionPerformed(ActionEvent event)
+      {
+         String string = "";
+         if (event.getSource() == textField1)
+            string = String.format("textField1: %s",
+               event.getActionCommand());
+
+         else if (event.getSource() == textField2)
+            string = String.format("textField2: %s",
+               event.getActionCommand());
+
+         else if (event.getSource() == textField3)
+            string = String.format("textField3: %s", 
+               event.getActionCommand());
+
+         else if (event.getSource() == passwordField)
+            string = String.format("passwordField: %s", 
+               event.getActionCommand());
+
+         JOptionPane.showMessageDialog(null, string);
+      }
+   }
+}
