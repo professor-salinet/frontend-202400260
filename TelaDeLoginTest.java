226aa7f3f7f4e532cc2e9a38546b81d2c9fde0e0
Projects repositories of Professor Salinet (SENAC) <rodrigo.silva@docente.pr.senac.br>
2024-09-17 09:41:41 -0300

    Add files via upload

---
+++ TelaDeLoginTest.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class TelaDeLoginTest extends JFrame
+{
+    public static void main(String[] args) {
+        TelaDeCadastro appTelaDeCadastro = new TelaDeCadastro();
+        appTelaDeCadastro.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
