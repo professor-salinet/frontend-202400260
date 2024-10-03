658d1d6020a21b8c4add290f7e89341d229ddbc5
Projects repositories of Professor Salinet (SENAC) <rodrigo.silva@docente.pr.senac.br>
2024-10-03 11:46:05 -0300

    Delete TelaDeLoginTest.java

--- TelaDeLoginTest.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-
-public class TelaDeLoginTest extends JFrame
-{
-    public static void main(String[] args) {
-        TelaDeCadastro appTelaDeCadastro = new TelaDeCadastro();
-        appTelaDeCadastro.setDefaultCloseOperation(EXIT_ON_CLOSE);
-    }
-}
