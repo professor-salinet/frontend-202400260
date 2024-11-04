5b962779bf4a760d33674c28d7fdc703dbf8271f
Projects repositories of Professor Salinet (SENAC) <rodrigo.silva@docente.pr.senac.br>
2024-11-04 10:47:03 -0300

    Add files via upload

--- GridBagDemo.java
+++ GridBagDemo.java
@@ -1,29 +1,23 @@
-// Fig. 19.22: GridBagDemo.java
-// Demonstrating GridBagLayout.
-import javax.swing.JFrame;
+// importação de todos os componentes da biblioteca gráfica javax, do módulo swing
+import javax.swing.*;
 
-public class GridBagDemo
-{ 
-   public static void main(String[] args)
-   { 
-      GridBagFrame gridBagFrame = new GridBagFrame(); 
-      gridBagFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      gridBagFrame.setSize(300, 150); 
-      gridBagFrame.setVisible(true); 
-   } 
-} // end class GridBagDemo
-
-/**************************************************************************
- * (C) Copyright 1992-2014 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
- *************************************************************************/
+// declaração da classe
+public class GridBagDemo 
+// início do bloco de código da classe
+{
+    // declaração do método executor principal
+    public static void main(String[] args) 
+    // início do bloco de código do método
+    {
+        // declaração da variável para inicialização do construtor da classe GridBagFrame
+        GridBagFrame gridBagFrame = new GridBagFrame();
+        // definição da operação padrão de fechamento: sair ao fechar
+        gridBagFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // definição do tamanho da tela
+        gridBagFrame.setSize(300, 150);
+        // definição da visualização da janela (frame)
+        gridBagFrame.setVisible(true);
+    // fim do bloco de código do método
+    }
+// fim do bloco de código da classe
+}
--- GridBagFrame.java
+++ GridBagFrame.java
@@ -1,94 +1,85 @@
-// Fig. 19.21: GridBagFrame.java
-// Demonstrating GridBagLayout.
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Component;
-import javax.swing.JFrame;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
+// importação dos componentes/classes
+import java.awt.*;
+import javax.swing.*;
 
-public class GridBagFrame extends JFrame 
-{ 
-   private final GridBagLayout layout; // layout of this frame
-   private final GridBagConstraints constraints; // layout's constraints
-    
-   // set up GUI
-   public GridBagFrame()
-   {
-      super("GridBagLayout");
-      layout = new GridBagLayout();
-      setLayout(layout); // set frame layout
-      constraints = new GridBagConstraints(); // instantiate constraints
+// declaração da classe e herança
+public class GridBagFrame extends JFrame {
 
-      // create GUI components
-      JTextArea textArea1 = new JTextArea("TextArea1", 5, 10);
-      JTextArea textArea2 = new JTextArea("TextArea2", 2, 2);
+    // declaração da variável de classe do layout
+    private final GridBagLayout layout;
+    // declaração da variável de classe das restrições / regras do componente
+    private final GridBagConstraints constraints;
 
-      String[] names = {"Iron", "Steel", "Brass"};
-      JComboBox<String> comboBox = new JComboBox<String>(names);
+    // declaração do construtor
+    public GridBagFrame() {
+        super("GridBagLayout");
+        // atribuição/instanciamento do objeto/layout GridBagLayout
+        layout = new GridBagLayout();
+        // definição do layout, conforme objeto atribuído acima
+        setLayout(layout); // set frame layout
+        // atribuição/instanciamento do objeto/restrições GridBagConstraints
+        constraints = new GridBagConstraints();
 
-      JTextField textField = new JTextField("TextField");
-      JButton button1 = new JButton("Button 1");
-      JButton button2 = new JButton("Button 2");
-      JButton button3 = new JButton("Button 3");
+        // atribuição/instanciamento dos elementos visuais
 
-      // weightx and weighty for textArea1 are both 0: the default
-      // anchor for all components is CENTER: the default
-      constraints.fill = GridBagConstraints.BOTH;
-      addComponent(textArea1, 0, 0, 1, 3);    
-       
-      // weightx and weighty for button1 are both 0: the default
-      constraints.fill = GridBagConstraints.HORIZONTAL;
-      addComponent(button1, 0, 1, 2, 1);
-      
-      // weightx and weighty for comboBox are both 0: the default
-      // fill is HORIZONTAL
-      addComponent(comboBox, 2, 1, 2, 1);             
+        JTextArea textArea1 = new JTextArea("TextArea1", 5, 10);
+        JTextArea textArea2 = new JTextArea("TextArea2", 2, 2);
 
-      // button2
-      constraints.weightx = 1000;  // can grow wider
-      constraints.weighty = 1;     // can grow taller
-      constraints.fill = GridBagConstraints.BOTH;
-      addComponent(button2, 1, 1, 1, 1);
-       
-      // fill is BOTH for button3
-      constraints.weightx = 0;
-      constraints.weighty = 0;    
-      addComponent(button3, 1, 2, 1, 1);
-       
-      // weightx and weighty for textField are both 0, fill is BOTH
-      addComponent(textField, 3, 0, 2, 1);
+        String[] names = { "Iron", "Steel", "Brass" };
+        JComboBox<String> comboBox = new JComboBox<String>(names);
 
-      // weightx and weighty for textArea2 are both 0, fill is BOTH
-      addComponent(textArea2, 3, 2, 1, 1);
-   } // end GridBagFrame constructor
+        JTextField textField = new JTextField("TextField");
+        JButton button1 = new JButton("Button 1");
+        JButton button2 = new JButton("Button 2");
+        JButton button3 = new JButton("Button 3");
 
-   // method to set constraints on 
-   private void addComponent(Component component,
-      int row, int column, int width, int height)
-   {
-      constraints.gridx = column; 
-      constraints.gridy = row;
-      constraints.gridwidth = width;
-      constraints.gridheight = height;
-      layout.setConstraints(component, constraints); // set constraints
-      add(component); // add component
-   }
-} // end class GridBagFrame
+        // definição da restrição/regra de layout (BOTH = ambas, significa que será "mesclada" em ambas direções, coluna e linha)
+        constraints.fill = GridBagConstraints.BOTH;
+        /**
+         * Adicionamento do elemento visual textArea1 e configurações do mesmo, onde:
+         * o primeiro parâmetro é o objeto que será adicionado/renderizado ao frame
+         * o segundo parâmetro é a definição de qual linha será renderizado na grade
+         * o terceiro parâmetro é a definição de qual coluna será renderizado na grade
+         * o quarto parâmetro é o tamanho da largura
+         * o quinto parâmetro é o tamanho da altura
+         */
+        addComponent(textArea1, 0, 0, 1, 3);
 
-/**************************************************************************
- * (C) Copyright 1992-2014 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
- *************************************************************************/
+        // definição da restrição/regra de layout (HORIZONTAL = lateral, significa que será "mesclada" apenas na direção lateral, coluna)
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        addComponent(button1, 0, 1, 2, 1);
+
+        addComponent(comboBox, 2, 1, 2, 1);
+
+        // definição do tamanho da largura do próximo elemento gráfico a ser renderizado no frame
+        constraints.weightx = 1000;
+        // definição do tamanho da altura do próximo elemento gráfico a ser renderizado no frame
+        constraints.weighty = 1;
+        constraints.fill = GridBagConstraints.BOTH;
+        addComponent(button2, 1, 1, 1, 1);
+
+        constraints.weightx = 0;
+        constraints.weighty = 0;
+        addComponent(button3, 1, 2, 1, 1);
+
+        addComponent(textField, 3, 0, 2, 1);
+
+        addComponent(textArea2, 3, 2, 1, 1);
+    }
+
+    // declaração do método addComponent, de uso exclusivo da classe GridBagFrame, sem retorno, apenas para adicionamento ao frame
+    private void addComponent(Component component, int row, int column, int width, int height) {
+        // definição da localização vertical do elemento na grade
+        constraints.gridy = row;
+        // definição da localização horizontal do elemento na grade
+        constraints.gridx = column;
+        // definição do tamanho da largura do elemento na grade
+        constraints.gridwidth = width;
+        // definição do tamanho da altura do elemento na grade
+        constraints.gridheight = height;
+        // definição da restrição/regra do componente/elemento gráfico
+        layout.setConstraints(component, constraints);
+        // adicionamento do componente já completo e pronto para renderização
+        add(component);
+    }
+}
